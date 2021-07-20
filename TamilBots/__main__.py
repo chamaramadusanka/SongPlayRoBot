a39a6a16c5286208ac49c66216b1012ad35bb964
chamaramadusanka <87709883+chamaramadusanka@users.noreply.github.com>
2021-07-20 16:28:30 +0100

    Update __main__.py

--- TamilBots/__main__.py
+++ TamilBots/__main__.py
@@ -11,13 +11,13 @@ from TamilBots.sql.chat_sql import add_chat_to_db
 start_text = """
 👋 𝗛𝗲𝗹𝗹𝗼 [{}](tg://user?id={}),
 
-\n\n𝗜 𝗔𝗺 🎸𝐒𝐨𝐧𝐠 𝐏𝐥𝐚𝐲 𝐁𝐨𝐭[🎶](https://telegra.ph/file/6cb884fe1cb943ec12df1.mp4)
+\n\n𝗜 𝗔𝗺 🎸MuSic Downloder BOt [🎶](https://telegra.ph/file/b3795f557f8d888140653.mp4)
 
-I'M Music Bot By @TamilBots 🤖
+I'M Music Bot By @MusicOFHuB
 
 𝗦𝗲𝗻𝗱 𝗧𝗵𝗲 𝗡𝗮𝗺𝗲 𝗢𝗳 𝗧𝗵𝗲 𝗦𝗼𝗻𝗴 𝗬𝗼𝘂 𝗪𝗮𝗻𝘁... 😍🥰🤗
 
-𝐄𝐠. ```/song Faded```
+𝐄𝐠. ```/song Bulleya```
 """
 
 owner_help = """
@@ -36,7 +36,7 @@ async def start(client, message):
     name = message.from_user["first_name"]
     if message.chat.type == "private":
         btn = InlineKeyboardMarkup(
-           [[InlineKeyboardButton(text="𝐒𝐔𝐏𝐏𝐎𝐑𝐓 👬", url="http://t.me/TamilSupport"),
+           [[InlineKeyboardButton(text="GROUP 👬", url="https://t.me/HuBOFMusic"),
              InlineKeyboardButton(
                         text="𝐀𝐃𝐃 𝐌𝐄 🤗", url="http://t.me/SongPlayRoBot?startgroup=true"
                     )
